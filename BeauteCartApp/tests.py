c91d87caf6a506c6866ca5d0ab2f087918d0f526
Nandana R.S <nandanars2024b@mca.ajce.in>
2023-12-06 06:58:43 +0530

    ninth commit

--- BeauteCartApp/tests.py
+++ BeauteCartApp/tests.py
@@ -1,55 +1,58 @@
 from django.test import TestCase
 
 # Create your tests here.
-import unittest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
-import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
-class LoginTest(unittest.TestCase):
-    def setUp(self):
-        # Start the Selenium WebDriver
-        self.driver = webdriver.Chrome()  # Adjust based on your WebDriver configuration
-        self.driver.get("http://127.0.0.1:8000/login/")  # Replace with the actual URL of your login page
+#test1 nrs
+# import unittest
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
 
-    def test_login_successful(self):
-        # Find the username, password, and login button elements
-        username_input = self.driver.find_element(By.NAME, "username")
-        password_input = self.driver.find_element(By.NAME, "password")
+# import time
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+
+
+# class LoginTest(unittest.TestCase):
+#     def setUp(self):
+#         # Start the Selenium WebDriver
+#         self.driver = webdriver.Chrome()  # Adjust based on your WebDriver configuration
+#         self.driver.get("http://127.0.0.1:8000/login/")  # Replace with the actual URL of your login page
+
+#     def test_login_successful(self):
+#         # Find the username, password, and login button elements
+#         username_input = self.driver.find_element(By.NAME, "username")
+#         password_input = self.driver.find_element(By.NAME, "password")
         
-        # You can use a more robust selector for the login button
-        login_button = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
-        )
+#         # You can use a more robust selector for the login button
+#         login_button = WebDriverWait(self.driver, 10).until(
+#             EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
+#         )
 
-        # Enter valid credentials
-        username_input.send_keys("Anna")
-        password_input.send_keys("Anna@123")
+#         # Enter valid credentials
+#         username_input.send_keys("Anna")
+#         password_input.send_keys("Anna@123")
 
-        # Click the login button
-        login_button.click()
+#         # Click the login button
+#         login_button.click()
 
-        # Wait for a while to see the result (you can adjust this based on your application's response time)
-        time.sleep(2)
+#         # Wait for a while to see the result (you can adjust this based on your application's response time)
+#         time.sleep(2)
         
-        # Assert that the URL matches the expected redirect
-        self.assertEqual(self.driver.current_url, 'http://127.0.0.1:8000/home2/')
+#         # Assert that the URL matches the expected redirect
+#         self.assertEqual(self.driver.current_url, 'http://127.0.0.1:8000/home2/')
 
-    def tearDown(self):
-        # Close the browser window
-        self.driver.close()
-
-
-if __name__ == "__main__":
-    unittest.main()
+#     def tearDown(self):
+#         # Close the browser window
+#         self.driver.close()
 
 
-# #test2
+# if __name__ == "__main__":
+#     unittest.main()
+
+
+#test2 nrs
 # from selenium import webdriver
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
@@ -91,50 +94,107 @@ if __name__ == "__main__":
 #     driver.quit()
 
 
-#test3
+
+
+
+#test3_add product
 # import unittest
 # from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import Select
 # from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import Select
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from django.urls import reverse  # Import reverse from django.urls
 
 # class AddProductTest(unittest.TestCase):
+
 #     def setUp(self):
-#         self.driver = webdriver.Chrome()  # or webdriver.Firefox() or other WebDriver
-#         self.driver.get("http://127.0.0.1:8000/add_product/")
+#         self.driver = webdriver.Chrome()
+#         self.driver.implicitly_wait(10)
+#         self.live_server_url = 'http://127.0.0.1:8000/'
 
 #     def tearDown(self):
 #         self.driver.quit()
 
+#     def login(self, username, password):
+#         driver = self.driver
+#         driver.get(self.live_server_url)
+
+#         # Find the login link and click on it
+#         login_link = driver.find_element(By.XPATH, "//a[text()='Login']")
+#         login_link.click()
+
+#         # Enter username and password
+#         username_input = driver.find_element(By.ID, "username")
+#         username_input.send_keys(username)
+
+#         password_input = driver.find_element(By.ID, "password")
+#         password_input.send_keys(password)
+
+#         # Submit the login form
+#         login_button = driver.find_element(By.CLASS_NAME, "login-button")
+#         login_button.click()
+
+#         # Wait for login to complete
+#         WebDriverWait(driver, 10).until(EC.url_changes(self.live_server_url))
+
 #     def test_add_product(self):
-#         # Fill in the form with sample data
-#         self.driver.find_element(By.ID, "productName").send_keys("Sample Product")
-#         self.driver.find_element(By.ID, "productDescription").send_keys("Sample product description.")
-#         self.driver.find_element(By.ID, "productPrice").send_keys("19.99")
-        
-#         # Select options from dropdowns
-#         Select(self.driver.find_element(By.ID, "productCategory")).select_by_visible_text("Skincare")
-#         Select(self.driver.find_element(By.ID, "productBrand")).select_by_visible_text("L'Oréal")
-#         Select(self.driver.find_element(By.ID, "productSeller")).select_by_index(1)  # Assuming there's at least one seller
+#         # Login first
+#         self.login("Naveen", "Naveen@123")
 
-#         self.driver.find_element(By.ID, "stockQuantity").send_keys("100")
+#         driver = self.driver
 
-#         # Upload an image (replace 'path/to/your/image.jpg' with the actual path)
-#         self.driver.find_element(By.ID, "productImage").send_keys("C:\\Users\\91730\\Desktop\\Cosmetics\\media\\product_images\\lakmefoundation1.PNG")
+#         # Access the add_product page using reverse
+#         add_product_url = reverse('add_product')
+#         driver.get(self.live_server_url + add_product_url)
 
-#         # Select a status
-#         Select(self.driver.find_element(By.ID, "productStatus")).select_by_visible_text("Active")
+#         # Wait for the form to load
+#         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "productName")))
 
-#         # Enable the "Add Product" button
-#         self.driver.find_element(By.ID, "addProductButton").click()
+#         # Fill out the form
+#         product_name = driver.find_element(By.ID, "productName")
+#         product_name.send_keys("Test Product")
 
-#         # Wait for the success modal to appear (you may need to adjust the wait time)
-#         success_modal = self.driver.find_element(By.ID, "successModal")
-#         self.assertTrue(success_modal.is_displayed(), "Success modal is not displayed")
+#         product_description = driver.find_element(By.ID, "productDescription")
+#         product_description.send_keys("This is a test product description.")
+
+#         product_price = driver.find_element(By.ID, "productPrice")
+#         product_price.send_keys("19.99")
+
+#         product_category = Select(driver.find_element(By.ID, "productCategory"))
+#         product_category.select_by_value("Face Makeup")
+
+#         product_subcategory = Select(driver.find_element(By.ID, "productSubcategory"))
+#         product_subcategory.select_by_value("Blush")
+
+#         product_brand = Select(driver.find_element(By.ID, "productBrand"))
+#         product_brand.select_by_value("L'Oréal")
+
+#         stock_quantity = driver.find_element(By.ID, "stockQuantity")
+#         stock_quantity.send_keys("30")
+
+#         product_image = driver.find_element(By.ID, "productImage")
+#         product_image.send_keys(r"C:\Users\91730\Desktop\Cosmetics\media\product_images\bluelady.png")
+
+#         product_seller = Select(driver.find_element(By.ID, "productSeller"))
+#         product_seller.select_by_index(1)
+
+#         product_status = Select(driver.find_element(By.ID, "productStatus"))
+#         product_status.select_by_value("active")
+
+#         # Submit the form
+#         add_product_button = driver.find_element(By.ID, "addProductButton")
+#         add_product_button.click()
+
+#         # Wait for the success modal to appear and assert its content
+#         success_modal = WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.ID, "successModal"))
+#         )
+#         success_message = success_modal.find_element(By.TAG_NAME, 'p').text
+#         self.assertEqual(success_message, 'PRODUCT ADDED SUCCESSFULLY')
 
 # if __name__ == "__main__":
 #     unittest.main()
 
 
-
-#test4
+#test4 userprofile.
